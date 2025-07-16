4c52fe63fa5451e0a90fff5f7e82713cc3f03875
guosenruan <gruan3@cps.edu>
2025-07-16 08:02:46 -0500

    build gradle update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunnerTest/CombinationTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunnerTest/CombinationTesting.java
@@ -2,33 +2,48 @@ package org.firstinspires.ftc.teamcode.RoadRunnerTest;
 
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
-import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
-import org.opencv.core.*;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.imgproc.Moments;
-import org.openftc.easyopencv.OpenCvCamera;
-import org.openftc.easyopencv.OpenCvCameraFactory;
-import org.openftc.easyopencv.OpenCvCameraRotation;
-import org.openftc.easyopencv.OpenCvPipeline;
+import org.openftc.apriltag.AprilTagDetection;
+import org.openftc.easyopencv.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@TeleOp(name = "OpenCV Color Detection")
+@TeleOp
+public class CombinationTesting extends LinearOpMode {
+    OpenCvCamera camera;
+    AprilTagDetectionPipeline aprilTagDetectionPipeline;
 
-public class OpenCVColorDetection extends LinearOpMode {
+    static final double FEET_PER_METER = 3.28084;
+    static final double INCH_PER_METER = 39.37;
 
-    double cX = 310.036;
-    double cY = 291.828;
+    double fx = 965.463;
+    double fy = 965.463;
+    double cx = 310.036;
+    double cy = 291.828;
     double width = 0;
+    double tagsize = 3 * 0.0254; //meters
+    int numFramesWithoutDetection = 0;
+    final float DECIMATION_HIGH = 3;
+    final float DECIMATION_LOW = 2;
+    final float THRESHOLD_HIGH_DECIMATION_RANGE_METERS = 1.0f;
+    final int THRESHOLD_NUM_FRAMES_NO_DETECTION_BEFORE_LOW_DECIMATION = 4;
 
     private OpenCvCamera controlHubCam;  // Use OpenCvCamera class from FTC SDK
     private static final int CAMERA_WIDTH = 640; // width  of wanted camera resolution
@@ -38,30 +53,86 @@ public class OpenCVColorDetection extends LinearOpMode {
     public static final double objectWidthInRealWorldUnits = 2.6;  // Replace with the actual width of the object in real-world units
     public static final double focalLength = 947.4038462;  // Replace with the focal length of the camera in pixels
 
-
     @Override
     public void runOpMode() {
+        aprilTagInitialize();
 
+        waitForStart();
+        telemetry.setMsTransmissionInterval(50);
+        while (opModeIsActive()) {
+            checkForAprilTag();
+
+            sleep(20);
+
+        }
+    }
+    public void aprilTagInitialize() {
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("CameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        aprilTagDetectionPipeline = new AprilTagDetectionPipeline(tagsize, fx, fy, cx, cy);
+
+        camera.setPipeline(aprilTagDetectionPipeline);
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                camera.startStreaming(640, 480, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int i) {
+
+            }
+        });
         initOpenCV();
         FtcDashboard dashboard = FtcDashboard.getInstance();
         telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
         FtcDashboard.getInstance().startCameraStream(controlHubCam, 30);
+    }
+    public void checkForAprilTag(){
+        ArrayList<AprilTagDetection> detections = aprilTagDetectionPipeline.getDetectionsUpdate();
 
+        if (detections != null) {
+            telemetry.addData("FPS", camera.getFps());
+            telemetry.addData("Overhead ms", camera.getOverheadTimeMs());
+            telemetry.addData("Pipeline ms", camera.getPipelineTimeMs());
 
-        waitForStart();
+            // If we don't see any tags
+            if (detections.size() == 0) {
+                numFramesWithoutDetection++;
 
-        while (opModeIsActive()) {
-            telemetry.addData("Coordinate", "(" + (int) cX + ", " + (int) cY + ")");
+                // If we haven't seen a tag for a few frames, lower the decimation
+                // so we can hopefully pick one up if we're e.g. far back
+                if (numFramesWithoutDetection >= THRESHOLD_NUM_FRAMES_NO_DETECTION_BEFORE_LOW_DECIMATION) {
+                    aprilTagDetectionPipeline.setDecimation(DECIMATION_LOW);
+                }
+            }
+            // We do see tags!
+            else {
+                numFramesWithoutDetection = 0;
+
+                // If the target is within 1 meter, turn on high decimation to
+                // increase the frame rate
+                if (detections.get(0).pose.z < THRESHOLD_HIGH_DECIMATION_RANGE_METERS) {
+                    aprilTagDetectionPipeline.setDecimation(DECIMATION_HIGH);
+                }
+
+                for (AprilTagDetection detection : detections) {
+                    Orientation rot = Orientation.getOrientation(detection.pose.R, AxesReference.INTRINSIC, AxesOrder.YXZ, AngleUnit.DEGREES);
+
+                    telemetry.addLine(String.format("\nDetected tag ID=%d", detection.id));
+                    telemetry.addLine(String.format("Translation X: %.2f inch", detection.pose.x * INCH_PER_METER));
+                    telemetry.addLine(String.format("Translation Y: %.2f incb", detection.pose.y * INCH_PER_METER));
+                    telemetry.addLine(String.format("Translation Z: %.2f inch", detection.pose.z * INCH_PER_METER));
+                    telemetry.addLine(String.format("Rotation Yaw: %.2f degrees", rot.firstAngle));
+                    telemetry.addLine(String.format("Rotation Pitch: %.2f degrees", rot.secondAngle));
+                    telemetry.addLine(String.format("Rotation Roll: %.2f degrees", rot.thirdAngle));
+                }
+            }
+            telemetry.addData("Coordinate", "(" + (int) cx + ", " + (int) cy + ")");
             telemetry.addData("Distance in Inch", (getDistance(width)));
             telemetry.update();
-
-            // The OpenCV pipeline automatically processes frames and handles detection
         }
-
-        // Release resources
-        controlHubCam.stopStreaming();
     }
-
     private void initOpenCV() {
 
         // Create an instance of the camera
@@ -72,7 +143,7 @@ public class OpenCVColorDetection extends LinearOpMode {
         controlHubCam = OpenCvCameraFactory.getInstance().createWebcam(
                 hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
 
-        controlHubCam.setPipeline(new YellowBlobDetectionPipeline());
+        controlHubCam.setPipeline(new CombinationTesting.YellowBlobDetectionPipeline());
 
         controlHubCam.openCameraDevice();
         controlHubCam.startStreaming(CAMERA_WIDTH, CAMERA_HEIGHT, OpenCvCameraRotation.UPRIGHT);
@@ -99,19 +170,19 @@ public class OpenCVColorDetection extends LinearOpMode {
 
                 // Display the width next to the label
                 String widthLabel = "Width: " + (int) width + " pixels";
-                Imgproc.putText(input, widthLabel, new Point(cX + 10, cY + 20), Imgproc.FONT_HERSHEY_SIMPLEX, 0.5, new Scalar(0, 255, 0), 2);
+                Imgproc.putText(input, widthLabel, new Point(cx + 10, cy + 20), Imgproc.FONT_HERSHEY_SIMPLEX, 0.5, new Scalar(0, 255, 0), 2);
                 //Display the Distance
                 String distanceLabel = "Distance: " + String.format("%.2f", getDistance(width)) + " inches";
-                Imgproc.putText(input, distanceLabel, new Point(cX + 10, cY + 60), Imgproc.FONT_HERSHEY_SIMPLEX, 0.5, new Scalar(0, 255, 0), 2);
+                Imgproc.putText(input, distanceLabel, new Point(cx + 10, cy + 60), Imgproc.FONT_HERSHEY_SIMPLEX, 0.5, new Scalar(0, 255, 0), 2);
                 // Calculate the centroid of the largest contour
                 Moments moments = Imgproc.moments(largestContour);
-                cX = moments.get_m10() / moments.get_m00();
-                cY = moments.get_m01() / moments.get_m00();
+                cx = moments.get_m10() / moments.get_m00();
+                cy = moments.get_m01() / moments.get_m00();
 
                 // Draw a dot at the centroid
-                String label = "(" + (int) cX + ", " + (int) cY + ")";
-                Imgproc.putText(input, label, new Point(cX + 10, cY), Imgproc.FONT_HERSHEY_COMPLEX, 0.5, new Scalar(0, 255, 0), 2);
-                Imgproc.circle(input, new Point(cX, cY), 5, new Scalar(0, 255, 0), -1);
+                String label = "(" + (int) cx + ", " + (int) cy + ")";
+                Imgproc.putText(input, label, new Point(cx + 10, cy), Imgproc.FONT_HERSHEY_COMPLEX, 0.5, new Scalar(0, 255, 0), 2);
+                Imgproc.circle(input, new Point(cx, cy), 5, new Scalar(0, 255, 0), -1);
 
             }
 
@@ -154,12 +225,12 @@ public class OpenCVColorDetection extends LinearOpMode {
             Rect boundingRect = Imgproc.boundingRect(contour);
             return boundingRect.width;
         }
-
     }
     private static double getDistance(double width){
         double distance = (objectWidthInRealWorldUnits * focalLength) / width;
         return distance;
     }
 
-
 }
+
+
